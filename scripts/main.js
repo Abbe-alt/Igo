9d1a19ba4f0d88b72b7d2c051517d93ded652c7c
Abbe <abelreyles121@gmail.com>
2021-06-22 23:35:38 -0600

    My very first webpage

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+const myHeading = document.querySelector('h1'); /*querySelector() is a function*/
+myHeading.textContent = 'yamete kudasai!';
+let myVariable = '3 + 5';
+let iceCream = 'chocolate';
+if(iceCream === 'chocolate') {
+alert('Yay, I love chocolate iceCream');
+} else {
+alert('Awww, but chocolate is the best');
+} /*alert ('') it's a function*/
+{
+myVariable = document.querySelector('h1');
+alert('caquita fea'); 
+}

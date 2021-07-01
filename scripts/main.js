4c35f27bf71179d891a09f6a4cc1b8635cb8fc57
Abbe <abelreyles121@gmail.com>
2021-06-30 22:17:40 -0600

    Web chetada

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 const myHeading = document.querySelector('h1'); /*querySelector('') is a function*/
 myHeading.textContent = 'Yamete Kudasai!';
-let myVariable = '3 + 5';
 let iceCream = 'chocolate';
 if(iceCream === 'chocolate') {
 alert('Yay, I love chocolate iceCream');
@@ -8,6 +7,34 @@ alert('Yay, I love chocolate iceCream');
 alert('Awww, but chocolate is the best');
 } /*alert ('') it's a function*/
 {
-myVariable = document.querySelector('h1');
-alert('caquita fea'); 
+//myVariable = document.querySelector('h1');
+//alert('caquita fea');
 }
+var el_up = document.getElementById("GFG_UP");
+            var el_down = document.getElementById("GFG_DOWN");
+            var str = "Click on button to change the background color";
+          
+            el_up.innerHTML = str;
+          
+            function changeColor(color) {
+                document.body.style.background = color;
+            }
+              
+            function gfg_Run() {
+                changeColor('grey');
+                el_down.innerHTML = "Background Color changed";
+            }
+            var el_up = document.getElementById("GFG_UP");
+            var el_down = document.getElementById("GFG_DOWN");
+            var str = "Click on button to change the background color";
+          
+            el_up.innerHTML = str;
+          
+            function changeColor(color) {
+                document.body.style.background = color;
+            }
+              
+            function gfg_Runn() {
+                changeColor('white');
+                el_down.innerHTML = "Background Color changed";
+            }
